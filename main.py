0341f42b25d2525fa2d6430c265f9b4a2ff398af
György Végh <gyorgy.vegh.job@gmail.com>
2022-06-08 22:55:10 +0200

    Green

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 def next_bigger(digits):
-    return -1
+    s_digits = str(digits)
+    l_digits = list(s_digits)
+    c = -1
+    for i in reversed(range(len(l_digits))):
+        if c >= 0:
+            break
+        for j in reversed(range(i)):
+            if int(l_digits[i]) > int(l_digits[j]):
+                print(l_digits[j:len(l_digits)])
+                bigger_digits = [int(k) for k in l_digits[j:len(l_digits)] if int(l_digits[j]) < int(k)]
+                m = min(bigger_digits)
+                print(bigger_digits)
+                print(m) 
+                last_digits = [int(k) for k in l_digits[j:len(l_digits)]]
+                last_digits.remove(m)
+                last_digits.sort()
+                print('After sort: ',last_digits)
+                c = m     
+                l_digits[j] = str(m)
+                n=0
+                for k in range(i,len(l_digits)):
+                    l_digits[k] = str(last_digits[n])
+                    n += 1
+                print(l_digits)
+                break
+            else:
+                break
+    return int(''.join(l_digits)) if c != -1 else c
--- test_main.py
+++ test_main.py
@@ -13,5 +13,16 @@ def test_next_bigger_4():
     assert next_bigger(414) == 441
 
 def test_next_bigger_5():
-    assert next_bigger(144) == 144
+    assert next_bigger(144) == 414
 
+def test_next_bigger_6():
+    assert next_bigger(1234567908) == 1234567980
+
+def test_next_bigger_7():
+    assert next_bigger(111) == -1
+
+def test_next_bigger_8():
+    assert next_bigger(1234567890) == 1234567908
+
+def test_next_bigger_9():
+    assert next_bigger(59884848459853) == 59884848483559
